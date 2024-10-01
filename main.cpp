ebda390ab0f51c50ef10000ec228a53701a25c76
Irfan Ansari <irfanbtt786@gmail.com>
2024-10-01 09:21:29 +0530

    working on clone feature

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+#define PROJECT_NAME "lit";
 
+int main(int argc, char *argv[]) {
+    
+    if(argc < 2) {
+        cout << "clone     Clone a repository into a new directory";
+    }
+    else if(argv[1] == "clone") {
+        string url = argv[2];
+        
+    }
 
 
     return 0;
